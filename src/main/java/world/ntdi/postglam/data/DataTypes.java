ca5833dc35e127908c156095e8381c0f58d682fe
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-02-16 18:04:55 -0500

    updated way of setting value to be more safe. I hate my users.

--- src/main/java/world/ntdi/postglam/data/DataTypes.java
+++ src/main/java/world/ntdi/postglam/data/DataTypes.java
@@ -4,57 +4,78 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
+import java.security.InvalidParameterException;
+
 /**
  * Refer to <a href="https://www.postgresql.org/docs/current/datatype.html">Postgresql Doc</a> for an explanation on each datatype.
  */
 public enum DataTypes {
-    BIGINT("bigint"),
-    BIGSERIAL("bigserial"),
-    BIT("bit"),
-    BOOLEAN("boolean"),
-    BOX("box"),
-    BYTEA("bytea"),
-    CHARACTER("character"),
-    CIDR("cidr"),
-    CIRCLE("circle"),
-    DATE("date"),
-    DOUBLE_PRECISION("double precision"),
-    INET("inet"),
-    INTEGER("integer"),
-    INTERVAL("interval"),
-    JSON("json"),
-    JSONB("jsonb"),
-    LINE("line"),
-    LSEG("lseg"),
-    MACADDR("macaddr"),
-    MACADDR8("macaddr8"),
-    MONEY("money"),
-    PATH("path"),
-    PG_LSN("pg_lsn"),
-    PG_SNAPSHOT("pg_snapshot"),
-    POINT("point"),
-    POLYGON("polygon"),
-    REAL("real"),
-    SMALLINT("smallint"),
-    SMALLSERIAL("smallserial"),
-    SERIAL("serial"),
-    TEXT("text"),
-    TSQUERY("tsquery"),
-    TSVECTOR("tsvector"),
-    TXID_SNAPSHOT("txid_snapshot"),
-    UUID("uuid"),
-    XML("xml");
+    BIGINT("bigint", false),
+    BIGSERIAL("bigserial", false),
+    BIT("bit", true),
+    VAR_BIT("varbit", true),
+    BOOLEAN("boolean", false),
+    BOX("box", false),
+    BYTEA("bytea", false),
+    CHARACTER("char", true),
+    VAR_CHARACTER("varchar", true),
+    CIDR("cidr", false),
+    CIRCLE("circle", false),
+    DATE("date", false),
+    DOUBLE_PRECISION("double precision", false),
+    INET("inet", false),
+    INTEGER("integer", false),
+    INTERVAL("interval", true),
+    JSON("json", false),
+    JSONB("jsonb", false),
+    LINE("line", false),
+    LSEG("lseg", false),
+    MACADDR("macaddr", false),
+    MACADDR8("macaddr8", false),
+    MONEY("money", false),
+    PATH("path", false),
+    PG_LSN("pg_lsn", false),
+    PG_SNAPSHOT("pg_snapshot", false),
+    POINT("point", false),
+    POLYGON("polygon", false),
+    REAL("real", false),
+    SMALLINT("smallint", false),
+    SMALLSERIAL("smallserial", false),
+    SERIAL("serial", false),
+    TIME("time", true),
+    TEXT("text", false),
+    TSQUERY("tsquery", false),
+    TSVECTOR("tsvector", false),
+    TXID_SNAPSHOT("txid_snapshot", false),
+    UUID("uuid", false),
+    XML("xml", false);
 
+    /* Name of the SQL form of the data type */
     private final String name;
+    /* If the data type can take a defined value */
+    private final boolean valuable;
+    /* The set value for that data type */
     @Getter @Setter
     private String value;
-    DataTypes(@NonNull final String name) {
+    DataTypes(@NonNull final String name, final boolean valuable) {
         this.name = name;
+        this.valuable = valuable;
     }
 
+    /**
+     * Stringify the SQL form of each datatype.
+     * <b>Note:</b> some data types are exclusive to Postgresql.
+     * If there is a value given, this method will generate that as well.
+     * Example: {@code char(36)} which is the equivalent to {@code uuid}.
+     *
+     * @return the SQL form of each datatype.
+     */
     public String toString() {
         return (value != null ? name + "(" + value + ")" : name);
     }
 
-
+    public void setValue(@NonNull final String value) {
+        if (valuable) setValue(value);
+        else throw new InvalidParameterException("Cannot apply a value to this data type");
+    }
 }
