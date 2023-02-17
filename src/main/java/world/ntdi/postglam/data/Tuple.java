eb76f2997b460ffd90022bfbc7025ff068b865a0
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-02-16 21:36:05 -0500

    Tuple!

--- src/main/java/world/ntdi/postglam/data/Tuple.java
+++ src/main/java/world/ntdi/postglam/data/Tuple.java
@@ -2,6 +2,11 @@ package world.ntdi.postglam.data;
 
 import lombok.Getter;
 
+/**
+ * A Tuple
+ * @param <A> Type Value A
+ * @param <B> Type Value B
+ */
 public class Tuple<A, B> {
 
     @Getter
@@ -10,6 +15,13 @@ public class Tuple<A, B> {
     @Getter
     private final B b;
 
+    /**
+     * Create a new Tuple.
+     * This can be done like so {@code new Tuple<String, Boolean>("Hello World", true);}
+     * Then you can access either a or b, a being the First value or the Left and b being the Second value or the Right.
+     * @param a
+     * @param b
+     */
     public Tuple(A a, B b) {
         this.a = a;
         this.b = b;
