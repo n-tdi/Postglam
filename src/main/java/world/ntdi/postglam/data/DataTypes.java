877ceec26bb7022fc174e711e028bd4ec30d83bf
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-02-17 18:32:03 -0500

    Updated to checking if quotes were needed around the data type when referenced in value form.

--- src/main/java/world/ntdi/postglam/data/DataTypes.java
+++ src/main/java/world/ntdi/postglam/data/DataTypes.java
@@ -10,45 +10,44 @@ import java.security.InvalidParameterException;
  * Refer to <a href="https://www.postgresql.org/docs/current/datatype.html">Postgresql Doc</a> for an explanation on each datatype.
  */
 public enum DataTypes {
-    BIGINT("bigint", false),
-    BIGSERIAL("bigserial", false),
-    BIT("bit", true),
-    VAR_BIT("varbit", true),
-    BOOLEAN("boolean", false),
-    BOX("box", false),
-    BYTEA("bytea", false),
-    CHARACTER("char", true),
-    VAR_CHARACTER("varchar", true),
-    CIDR("cidr", false),
-    CIRCLE("circle", false),
-    DATE("date", false),
-    DOUBLE_PRECISION("double precision", false),
-    INET("inet", false),
-    INTEGER("integer", false),
-    INTERVAL("interval", true),
-    JSON("json", false),
-    JSONB("jsonb", false),
-    LINE("line", false),
-    LSEG("lseg", false),
-    MACADDR("macaddr", false),
-    MACADDR8("macaddr8", false),
-    MONEY("money", false),
-    PATH("path", false),
-    PG_LSN("pg_lsn", false),
-    PG_SNAPSHOT("pg_snapshot", false),
-    POINT("point", false),
-    POLYGON("polygon", false),
-    REAL("real", false),
-    SMALLINT("smallint", false),
-    SMALLSERIAL("smallserial", false),
-    SERIAL("serial", false),
-    TIME("time", true),
-    TEXT("text", false),
-    TSQUERY("tsquery", false),
-    TSVECTOR("tsvector", false),
-    TXID_SNAPSHOT("txid_snapshot", false),
-    UUID("uuid", false),
-    XML("xml", false);
+    BIGINT("bigint", false, false),
+    BIGSERIAL("bigserial", false, false),
+    BIT("bit", true, false),
+    VAR_BIT("varbit", true, false),
+    BOOLEAN("boolean", false, false),
+    BOX("box", false, false),
+    BYTEA("bytea", false, false),
+    CHARACTER("char", true, true),
+    VAR_CHARACTER("varchar", true, true),
+    CIDR("cidr", false, true),
+    CIRCLE("circle", false, true),
+    DATE("date", false, true),
+    DOUBLE_PRECISION("double precision", false, false),
+    INET("inet", false, true),
+    INTEGER("integer", false, false),
+    INTERVAL("interval", true, false),
+    JSON("json", false, true),
+    JSONB("jsonb", false, true),
+    LINE("line", false, true),
+    LSEG("lseg", false, true),
+    MACADDR("macaddr", false, true),
+    MACADDR8("macaddr8", false, true),
+    MONEY("money", false, false),
+    PATH("path", false, true),
+    PG_LSN("pg_lsn", false, true),
+    PG_SNAPSHOT("pg_snapshot", false, true),
+    POINT("point", false, true),
+    POLYGON("polygon", false, true),
+    REAL("real", false, false),
+    SMALLINT("smallint", false, false),
+    SMALLSERIAL("smallserial", false, false),
+    SERIAL("serial", false, false),
+    TIME("time", true, true),
+    TEXT("text", false, true),
+    TSQUERY("tsquery", false, true),
+    TSVECTOR("tsvector", false, true),
+    UUID("uuid", false, true),
+    XML("xml", false, true);
 
     /**
      * Name of the SQL form of the data type
@@ -58,6 +57,12 @@ public enum DataTypes {
      * If the data type can take a defined value
      */
     private final boolean valuable;
+
+    /**
+     * If the data type needs to be surrounded in '' when written in sql
+     */
+    @Getter
+    private final boolean needsQuotes;
 
     /**
      * The set value for that data type
@@ -72,9 +77,10 @@ public enum DataTypes {
      */
     @Getter @Setter
     private boolean notNull = true;
-    DataTypes(@NonNull final String name, final boolean valuable) {
+    DataTypes(@NonNull final String name, final boolean valuable, final boolean needsQuotes) {
         this.name = name;
         this.valuable = valuable;
+        this.needsQuotes = needsQuotes;
     }
 
     /**
@@ -105,5 +111,17 @@ public enum DataTypes {
      */
     public String getNotNull() {
         return (notNull ? "NOT NULL" : "");
+    }
+
+    /**
+     * Checks if a targeted value needs quotes around it. This is useful when inserting items into a table.
+     * This method is meant just for table use.
+     *
+     * @param target The targeted string to adjust if needed
+     * @param dataType The data type that the string references
+     * @return A string with single quotes around it if the data type requests it
+     */
+    public static String needQuotes(String target, DataTypes dataType) {
+        return (dataType.isNeedsQuotes() ? "'" + target + "'" : target);
     }
 }
