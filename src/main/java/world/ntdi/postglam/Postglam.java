66bc8f287e1778a5ce1fa2d4126e8e9ab0276c7a
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-02-17 18:34:21 -0500

    Moved method of Logger in honor of Logging.java

--- src/main/java/world/ntdi/postglam/Postglam.java
+++ src/main/java/world/ntdi/postglam/Postglam.java
@@ -1,16 +1,13 @@
 package world.ntdi.postglam;
 
-import lombok.Getter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import world.ntdi.postglam.data.DataTypes;
+import world.ntdi.postglam.sql.helper.SQLTableHelper;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class Postglam {
-    /**
-     * Makes use of SLF4J to create a logger based on the class you're in
-     * @param clazz Class you're logging from
-     * @return Returns the Logger from LoggerFactory
-     * @param <T> Type Parameter
-     */
-    public static <T> Logger getLogger(Class<T> clazz) {
-        return LoggerFactory.getLogger(clazz);
-    }
+
+
 }
